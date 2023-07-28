90be15ca257f03c8c0486e63acbca13882082600
Jesus Aquino <soluciones@jaqconsultor.com>
2023-07-28 17:58:11 -0400

    FrameWork Openxava

---
+++ workspace/facturacion/src/main/java/com/tuempresa/facturacion/anotaciones/ISBN.java
@@ -0,0 +1,17 @@
+package com.tuempresa.facturacion.anotaciones; // En el paquete 'anotaciones'
+ 
+import java.lang.annotation.*;
+
+import javax.validation.*;
+ 
+@Constraint(validatedBy = com.tuempresa.facturacion.validadores.ValidadorISBN.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ISBN {
+	
+	boolean buscar() default true; // Para (des)activar la búsqueda web al validar
+	
+    Class<?>[] groups() default{};
+    Class<? extends Payload>[] payload() default{};
+    String message() default "isbn_invalido"; // Id del mensaje en el archivo i18n
+}
